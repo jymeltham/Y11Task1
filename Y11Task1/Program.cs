c8a051c17ab77c8fc4d953f6c95df56b2a565f46
Jay Muirhead <jym@eltham-college.org.uk>
2021-03-23 08:12:14 +0000

    Add project files.

---
+++ Y11Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Y11Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Two tasks - choose your task!
+             * 
+             * Happy Numbers 
+             * A happy number is defined by the following process: 
+             * Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+             * and repeat the process until the number equals 1 (where it will stay), or it loops 
+             * endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
+             * are happy numbers, while those that do not end in 1 are unhappy numbers. 
+             * Display an example of your output here. Find the first eight happy numbers.
+             * 
+             *  Mor-se Coding 
+             * Create a program that allows you to enter a string and encode it into Morse code, using ‘ . ‘ 
+             * and ‘ - ‘ notation. Spaces between words should be replaced with the “|” (pipe) character. 
+             * Use a normal space for gaps between each character.
+             * Extension:
+             * 1. Develop your program to translate from Morse to alphanumeric, using the standards above
+             * 
+             */
+        }
+    }
+}
